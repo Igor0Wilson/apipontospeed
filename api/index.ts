b18ab6aaa12a7bd728401343ac576e5840abfd89
Igor Wilson <igorwilsonsimiao@gmail.com>
2025-09-25 09:13:33 -0300

    primeiro commit

--- api/index.ts
+++ api/index.ts
@@ -7,10 +7,21 @@ app.register(cors, {
   origin: "*",
 });
 
-app.get("/", () => {
-  return "Hellow word";
+app.get("/api/ponto", async (request, reply) => {
+  return { message: "API funcionando (GET)" };
 });
 
+app.post("/api/ponto", async (request, reply) => {
+  try {
+    const ponto = request.body;
+    console.log("Dados recebidos:", ponto);
+
+    reply.send({ message: "Ponto salvo com sucesso" });
+  } catch (err) {
+    console.error("Erro ao salvar ponto:", err);
+    reply.status(500).send({ error: err.message });
+  }
+});
 app
   .listen({
     port: 3333,
